e87e63745218f813370c9b960a464eb411cc9906
Shashank Jaiswal <shashankjaiswal@dyn-160-39-224-254.dyn.columbia.edu>
2019-06-14 16:42:29 -0400

    pushing the html doc

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+var movies = ["Clueless", "Mean Girls", "Silver Linings Playbook", "Cinderella", "Aladdin"];
+
+    // Add buttons for original movies array
+    function renderButtons() {
+        $("#movie-buttons").empty();
+        for (i = 0; i < movies.length; i++) 
