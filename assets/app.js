77f7bce7a6e076df35beb6692b119221a100b600
Shashank Jaiswal <shashankjaiswal@Shashanks-MBP.fios-router.home>
2019-06-14 22:45:21 -0400

    added buttons and made div id's

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,35 @@
-var movies = ["Clueless", "Mean Girls", "Silver Linings Playbook", "Cinderella", "Aladdin"];
+
+ $(document).ready(function() {
+    var myCars = ["Audi", "Lamborgini", "BMW", "MiniCooper"];
 
     // Add buttons for original movies array
     function renderButtons() {
-        $("#movie-buttons").empty();
-        for (i = 0; i < movies.length; i++) 
+        $("#cars-buttons").empty();
+        for (i = 0; i < myCars.length; i++) {
+            $("#cars-buttons").append("<button class='btn btn-success' data-cars='" + cars[i] + "'>" + cars[i] + "</button>");
+        }
+
+        // Getting gifs from api... onto html
+        $("button").on("click", function() {
+            // console.log('hit')
+            var myCar = $(this).attr("data-myCar");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                car + "414VkDgoZW0Q9ClOjd9frIJsN18n7Cx4&limit=9"
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                var results = response.data;
+                $("#cars").empty();
+                for (var i = 0; i < results.length; i++) {
+                    var myCarDiv = $("<div>");
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var carImg = $("<img>");
+
+        
+
+
+    
+    
+});
